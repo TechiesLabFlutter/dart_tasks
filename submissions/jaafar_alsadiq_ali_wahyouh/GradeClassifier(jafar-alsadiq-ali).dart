bff04a9cfad3da6f35b1c82879e9675d493420ff
Jafar S.Ali <dshow398@gmail.com>
2026-09-07 16:23:35 +0300

    Add files via upload

---
+++ submissions/jaafar_alsadiq_ali_wahyouh/GradeClassifier(jafar-alsadiq-ali).dart
@@ -0,0 +1,52 @@
+
+//grade classifier in if/else statement Task Number 1 -->
+
+/*
+void main() {
+  int grade = 75;
+ if (grade >= 90) {
+  print('grade is A');
+ } else if (grade >=80) {
+  print('grade is B');
+ } else if (grade >=70) {
+  print('grade is C');
+ } else if (grade >=60) {
+  print('grade is D');
+ } else {
+  print('grade is F');}
+ }
+
+*/
+
+
+// Task Number 2 --> in switch statment
+
+/*
+void main() {
+  int score = 68;
+  if (score < 0 || score > 100) {
+  print('Invalid score');
+  } else {
+  int grade = score ~/ 10;
+  switch (grade) {
+  case 10:
+  case 9:
+  print('grade is A');
+  break;
+  case 8:
+  print('grade is B');
+  break;
+  case 7:
+  print('grade is C');
+  break;
+  case 6:
+  print('grade is D');
+  break;
+  default:
+  print('grade is F');
+  }
+  }
+}
+
+*/
+
