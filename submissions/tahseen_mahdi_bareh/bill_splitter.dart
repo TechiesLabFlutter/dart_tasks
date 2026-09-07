c438d7c0e2d2d34cb272e793a8bf60d9bbde51d3
tahseenmahdi <123564578+tahseenmahdi@users.noreply.github.com>
2026-09-08 00:57:16 +0300

    edit the third task

--- submissions/tahseen_mahdi_bareh/bill_splitter.dart
+++ submissions/tahseen_mahdi_bareh/bill_splitter.dart
@@ -1,8 +1,9 @@
 import "dart:io";
+
 void main() {
   int numberOfPeople;
   int bill;
-  int tip=10;
+  int tip = 10;
 
   print("number of people: ");
   numberOfPeople = int.parse(stdin.readLineSync()!);
@@ -10,19 +11,21 @@ void main() {
   print("bill: ");
   bill = int.parse(stdin.readLineSync()!);
 
-  int billAfterTip = (bill + (bill * tip / 100))~/1;
+  int billAfterTip = (bill + (bill * tip / 100)).toInt();
   int whatEachPersonPays = billAfterTip ~/ numberOfPeople;
-  int remainder = billAfterTip;
+  int unpaidAmount = billAfterTip % numberOfPeople;
 
-  print("each one pays: $whatEachPersonPays as the total bill after the tip is $billAfterTip");
+  print(
+    "each one pays: $whatEachPersonPays as the total bill after the tip is $billAfterTip",
+  );
 
-  for(int i=1; i<=numberOfPeople; i++){
-    remainder -= whatEachPersonPays;
-    print("Person $i pays: $whatEachPersonPays and the remainder is $remainder");
-    if (i>=20){
-      print("can not print more than 20 people ");
+  for (int i = 1; i <= numberOfPeople; i++) {
+    print("Person $i pays: $whatEachPersonPays");
+    if (i >= 20) {
+      print("can not print more than 20 people");
       break;
-      
     }
   }
+
+  print("Unpaid amount because of integer division: $unpaidAmount");
 }
