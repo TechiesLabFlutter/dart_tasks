738bfecc46bb968876fb4e25a9ccf5a7e10e4ea1
sajamohammedl <sajamohammed954@gmail.com>
2026-09-08 01:18:16 +0300

    Remove unnecessary blank lines in temp_conv.dart

--- submissions/saja_mohammed/temp_conv.dart
+++ submissions/saja_mohammed/temp_conv.dart
@@ -10,8 +10,6 @@ void main() {
   final String? input = stdin.readLineSync();
   final double celsius = double.parse(input!) ;    
   final double fahrenheit = (celsius * 9 / 5) + offset;
-
-  // طباعة النتيجة باستخدام String interpolation
   print('$celsius°C is equal to $fahrenheit°F');
 }
 
